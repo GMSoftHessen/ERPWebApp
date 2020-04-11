0739c66e693b2896e8a1cb5d23da7ae0fe28092f
karim <karim.eddarif@googlemail.com>
2020-04-11 16:13:31 +0200

    erste entwurf2

--- WebApp/ExtApp/app/store/sales/Customer.js
+++ WebApp/ExtApp/app/store/sales/Customer.js
@@ -11,7 +11,7 @@ Ext.define('EddkaApp.store.sales.Customer', {
         { name: 'Deanna',   email: "deanna.troi@enterprise.com",    phone: "555-333-3333" },
         { name: 'Data',     email: "mr.data@enterprise.com",        phone: "555-444-4444" }
     ]},
-
+        
     proxy: {
         type: 'memory',
         reader: {
